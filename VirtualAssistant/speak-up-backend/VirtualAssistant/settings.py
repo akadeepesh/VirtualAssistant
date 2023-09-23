703af41fcf8d80b495a027f0e20b01aaa6575ffc
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2023-09-24 04:54:24 +0530

    trying vercel

--- VirtualAssistant/speak-up-backend/VirtualAssistant/settings.py
+++ VirtualAssistant/speak-up-backend/VirtualAssistant/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".vercel.app", ".now.sh", "127.0.0.1", "localhost"]
 
 
 # Application definition
