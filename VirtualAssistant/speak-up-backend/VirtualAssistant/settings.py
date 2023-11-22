dabfde80a4066903a726fce46357a74e157845bc
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2023-11-23 02:21:00 +0530

    allowed hosts

--- VirtualAssistant/speak-up-backend/VirtualAssistant/settings.py
+++ VirtualAssistant/speak-up-backend/VirtualAssistant/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 
 SECURE_SSL_REDIRECT = False
